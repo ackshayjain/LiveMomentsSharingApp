504cd04fcd6cbb847dce0fc9af32d70fc25cbd0e
shaurya.puri.2016@live.rhul.ac.uk <Shaurya.Puri.2016@live.rhul.ac.uk>
2018-06-01 02:33:45 +0530

    React Navigation Installation
    Addition of the React Navigation module into the application.

--- TheApp/Components/WalkThrough.js
+++ TheApp/Components/WalkThrough.js
@@ -1,22 +1,15 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { createStackNavigator } from 'react-navigation'
-import WalkThrough from './Components/WalkThrough'
 
 export default class App extends React.Component {
   render() {
     return (
-      <AppStackNavigator />
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
     );
   }
 }
-
-const AppStackNavigator = createStackNavigator({
-
-  Main: {
-    screen: WalkThrough
-  } 
-})
 
 const styles = StyleSheet.create({
   container: {
