ede38b3a51bc8ad8e4ed235561f055679e35794d
shaurya.puri.2016@live.rhul.ac.uk <Shaurya.Puri.2016@live.rhul.ac.uk>
2018-06-01 18:37:02 +0530

    Walkthrough Screen progress
    Added Navigator and AppIntro Screen. Bug fix to be done.

--- TheApp/Components/WalkThrough.js
+++ TheApp/Components/WalkThrough.js
@@ -1,21 +1,63 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Alert } from 'react-native';
+import AppIntro from 'react-native-app-intro';
 
-export default class App extends React.Component {
-  render() {
+class WalkThrough extends Component {
+  
+  onSkipBtnHandle = (index) => {
+    Alert.alert('Skip');
+    console.log(index);
+  }
+  doneBtnHandle = () => {
+    Alert.alert('Done');
+  }
+  nextBtnHandle = (index) => {
+    Alert.alert('Next');
+    console.log(index);
+  }
+  onSlideChangeHandle = (index, total) => {
+    console.log(index, total);
+  }
+  render() { 
+      const pageArray = [{
+      title: 'Page 1',
+      description: 'Description 1',
+      backgroundColor: '#fa931d',
+      fontColor: '#fff',
+      level: 10,
+    }, {
+      title: 'Page 2',
+      description: 'Description 2',
+      backgroundColor: '#a4b602',
+      fontColor: '#fff',
+      level: 10,
+    }];  
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+      <AppIntro
+        style={{flex:1}}
+        onNextBtnClick={this.nextBtnHandle}
+        onDoneBtnClick={this.doneBtnHandle}
+        onSkipBtnClick={this.onSkipBtnHandle}
+        onSlideChange={this.onSlideChangeHandle}
+        pageArray={pageArray}
+      />   
     );
   }
 }
 
+export default WalkThrough;
+
 const styles = StyleSheet.create({
-  container: {
+  slide: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
-  }
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+    padding: 15,
+  },
+  text: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
 });
